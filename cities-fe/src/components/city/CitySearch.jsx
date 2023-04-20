23ee1fbad95025f292470af731f70bcb0743d200
aliturgutbozkurt <ali.turgut.bozkurt@gmail.com>
2023-04-20 13:35:39 +0300

    Unused code removed

--- cities-fe/src/components/city/CitySearch.jsx
+++ cities-fe/src/components/city/CitySearch.jsx
@@ -2,21 +2,10 @@ import React, {useState, useRef} from "react";
 import {Button, Form, Input} from "antd";
 
 function CitySearch(props) {
-    const [form] = Form.useForm();
-    const [formLayout, setFormLayout] = useState('horizontal');
     const onInputChange = (e) => {
         props.onChange(e.target.value);
     };
-
-    const buttonItemLayout =
-        formLayout === 'horizontal'
-            ? {
-                wrapperCol: {
-                    span: 14,
-                    offset: 4,
-                },
-            }
-            : null;
+    
     return (
         <div>
             <Input placeholder="Search By Name" onChange={(e) => onInputChange(e)}/>
