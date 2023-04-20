68dda4b5a5f1e4c5dc29b9157b95b8c2ae3325ae
aliturgutbozkurt <ali.turgut.bozkurt@gmail.com>
2023-04-20 11:40:30 +0300

    City controller path Bug fix

--- cities/src/main/java/com/kuehnenagel/cities/controller/CityController.java
+++ cities/src/main/java/com/kuehnenagel/cities/controller/CityController.java
@@ -10,7 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/cities")
 @RequiredArgsConstructor
 public class CityController {
 
